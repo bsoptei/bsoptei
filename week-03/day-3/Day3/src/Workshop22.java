02bd23ce0fc2a0603d17dada20454e1759b5ce51
bsoptei <bsoptei@gmail.com>
2016-11-08 15:15:47 +0100

    Monday update

--- week-03/day-3/Day3/src/Workshop22.java
+++ week-03/day-3/Day3/src/Workshop22.java
@@ -1,22 +1,14 @@
 import java.util.Scanner;
 
 public class Workshop22{
-    static Scanner newString = new Scanner(System.in);
     public static void main(String... args) {
         // create a function which prints the input String parameters (can have multiple number of arguments)
-        System.out.print("Enter your string: ");
-        String myString = newString.nextLine();
-        stringParam(myString);
+
+        stringParam("one", "two", "three");
     }
-    public static void stringParam(String inputString) {
-        inputString = inputString.trim();
-        if (inputString.isEmpty()) {
-            System.out.println("The string is empty.");
-        }
-        else {
-            System.out.printf("Length of the string is: %d characters \n", inputString.length());
-            System.out.printf("First character of the string is: %s\n", inputString.charAt(0));
-            System.out.printf("Last character of the string is: %s", inputString.charAt(inputString.length() - 1));
+    public static void stringParam(String... inputString) {
+        for (String s : inputString) {
+            System.out.println(s);
         }
     }
 }
