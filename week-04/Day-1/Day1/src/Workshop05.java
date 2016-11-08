5ce6fc404c111ab65254efcabd60f98b624dde1d
bsoptei <bsoptei@gmail.com>
2016-11-08 16:36:43 +0100

    Monday update

--- week-04/Day-1/Day1/src/Workshop05.java
+++ week-04/Day-1/Day1/src/Workshop05.java
@@ -4,9 +4,8 @@ public class Workshop05{
     public static void main(String... args){
         ArrayList<Integer> numList = new ArrayList<Integer>(Arrays.asList(12, 1, 532, 23, 0, 7, 54, 32));
         // Copy every element of "numList" to an Array
-        numList.toArray();
-        Integer[] numListCopy = new Integer[8];
-        System.arraycopy(numList.toArray(),0, numListCopy, 0, 8);
+        Integer[] numListCopy = numList.toArray(new Integer[0]);
+//        System.arraycopy(numList.toArray(),0, numListCopy, 0, 8);
         for (Integer element: numListCopy) {
             System.out.println(element);
         }
--- week-04/Day-1/Day1/src/Workshop16.java
+++ week-04/Day-1/Day1/src/Workshop16.java
@@ -4,11 +4,20 @@ public class Workshop16{
     public static void main(String... args){
         ArrayList<Integer> al = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7 ));
         // Fix this code fragment! It should remove every even number from the list.
-        for(int i = 0; i < al.size(); i++){
-            if(al.get(i) % 2 == 0){
-                al.remove(i);
+//        for(int i = 0; i < al.size(); i++){
+//            if(al.get(i) % 2 == 0){
+//                al.remove(i);
+//            }
+//        }
+
+        Iterator iterator = al.iterator();
+        while (iterator.hasNext()) {
+            int element = (Integer)iterator.next();
+            if (element % 2 == 0 ) {
+                iterator.remove();
             }
         }
+
         System.out.println(al);
     }
 }
