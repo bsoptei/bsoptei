5b7db13fc69f32fa0c0166371ce5a85dcc45f267
bsoptei <bsoptei@gmail.com>
2016-12-15 09:55:01 +0100

    added multiple addresses, it works!

--- week-08/day3/Workshop03/src/Workshop03.java
+++ week-08/day3/Workshop03/src/Workshop03.java
@@ -8,11 +8,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Created by Söp on 2016.12.14.. Create tables with foreign objects
  */
 public class Workshop03 {
+    private static ArrayList<Account> accounts = new ArrayList<>();
+
     public static void main(String[] args) throws SQLException, IOException {
         StringBuilder databaseUrl = new StringBuilder();
         databaseUrl.append("jdbc:mysql://127.0.0.1:3306/workshop03?user=root&password=");
@@ -24,16 +27,24 @@ public class Workshop03 {
         ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl.toString());
 
         Address address = new Address("Andrássy út", "Budapest", "1061", "Hungary");
-        Account account = new Account("Captain America", "uejnsd632**234.", address);
+        Address address2 = new Address("Kossuth Lajos u.", "Veszprém", "8200", "Hungary");
+        Address address3 = new Address("Veszprémi út", "Balatonalmádi", "8220", "Hungary");
+        Address address4 = new Address("Szív u.", "Budapest", "1063", "Hungary");
+
+        accounts.add(new Account("Captain America", "uejnsd632**234.", address));
+        accounts.add(new Account("Superman", "12345", address2));
+        accounts.add(new Account("Spiderman", "12345", address3));
+        accounts.add(new Account("Batman", "12345", address4));
 
         Dao<Account, String> accountDao = DaoManager.createDao(connectionSource, Account.class);
 
         TableUtils.createTableIfNotExists(connectionSource, Account.class);
         TableUtils.createTableIfNotExists(connectionSource, Address.class);
 
-        createAccountIfNotExists(accountDao, account);
-
-        // Experimenting with 2 types of querying
+        for (Account account : accounts) {
+            createAccountIfNotExists(accountDao, account);
+        }
+        //Experimenting with 2 types of querying
         System.out.println(accountDao.queryForEq("name", "Captain America").toString());
         System.out.println(accountDao.queryForId("Captain America"));
         connectionSource.close();
