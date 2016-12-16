1aea0f963f42cc56bd660739e80d0dec7520a743
bsoptei <bsoptei@gmail.com>
2016-12-16 09:42:12 +0100

    removed @Getter annotation from User class

--- week-08/project/ToDoApp_with_SQL/src/sample/User.java
+++ week-08/project/ToDoApp_with_SQL/src/sample/User.java
@@ -2,12 +2,10 @@ package sample;
 
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
-import lombok.Getter;
 
 @DatabaseTable(tableName = "users")
 class User {
     @DatabaseField(id = true)
-    @Getter
     private String name;
 
     public User() {
