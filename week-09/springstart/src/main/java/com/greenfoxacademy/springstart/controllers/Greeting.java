d511f1b9a462353bcdace298552d4c8ca72384fd
bsoptei <bsoptei@gmail.com>
2016-12-18 18:31:03 +0100

    rest app is working, with user name, and counting

---
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by Söp on 2016.12.18..
+ */
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    long id;
+    String name;
+}
