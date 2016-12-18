67fe81b2cdae8a759000fa24c65047875dd02d78
bsoptei <bsoptei@gmail.com>
2016-12-18 19:22:08 +0100

    web greet is working, counting also works

--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -5,15 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Created by Söp on 2016.12.18..
  */
 @Controller
 public class HelloWebController {
-
+    private final AtomicLong counter = new AtomicLong();
     @RequestMapping("web/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") final String name, Model model) {
+        Map<String, String> params = new HashMap<String, String>(){{
+            put("name", name);
+            put("count", String.valueOf(counter.incrementAndGet()));
+        }};
+        model.addAllAttributes(params);
         return "greeting";
     }
+
+
 }
