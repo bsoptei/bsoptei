6c15506a74077e70d6718eec85d349ced0f17e12
bsoptei <bsoptei@gmail.com>
2016-12-18 18:54:19 +0100

    web greet is working

---
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -0,0 +1,19 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Söp on 2016.12.18..
+ */
+@Controller
+public class HelloWebController {
+
+    @RequestMapping("web/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
