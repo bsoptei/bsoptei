ad31243953d787f58059764af15b3c6ddf7cf49b
bsoptei <bsoptei@gmail.com>
2016-12-19 09:20:05 +0100

    multiple language iteration worksgit add week-09!

---
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloMultiplier.java
@@ -0,0 +1,24 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Söp on 2016.12.18..
+ */
+@Controller
+public class HelloMultiplier {
+    private static final ArrayList<String> hellos = new ArrayList<>(Arrays.asList("Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"));
+    @RequestMapping("/web/multiple")
+    public String multiple(Model model) {
+        model.addAttribute("hellos", hellos);
+        return "multiple";
+    }
+}
